2d074e11fb1d290c06ad45f9ed478bdf355922f5
João Paulo <joao.santos@intelitrader.com.br>
2023-08-09 16:07:32 -0300

    adicionando mascara

--- js/app.js
+++ js/app.js
@@ -8,6 +8,18 @@ const ATIVO_TEXTO = {
     false:"Não"
 }
 
+const maskTelephone = (event) => {
+    let input = event.target
+    input.value = phoneMask(input.value)
+  }
+
+const phoneMask = (value) => {
+    if (!value) return ""
+    value = value.replace(/\D/g,'')
+    value = value.replace(/(\d{2})(\d)/,"($1) $2")
+    value = value.replace(/(\d)(\d{4})$/,"$1-$2")
+    return value
+}
 function formatarData(dataString) {
     const data = new Date(dataString);
     const dia = data.getDate().toString().padStart(2, '0')
